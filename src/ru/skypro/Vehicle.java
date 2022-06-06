f7b3e740b42b249af88995ac8186cc1569928e19
Vladchegg <vlad0277@mail.ru>
2022-06-06 18:20:05 +0300

    решенное домашнее задание 2.3

---
+++ src/ru/skypro/Vehicle.java
@@ -0,0 +1,36 @@
+package ru.skypro;
+
+public abstract class Vehicle {
+    private String modelName;
+    private int wheelsCount;
+
+    public Vehicle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "modelName='" + modelName + '\'' +
+                ", wheelsCount=" + wheelsCount +
+                '}';
+    }
+
+}
